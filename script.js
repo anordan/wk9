00766f005d42bbbad9c1a04cb53b31ff14740e7f
ashley nordan <abuxbaum@mcad.edu>
2017-10-29 20:21:51 -0500

    files
    sss

---
+++ script.js
@@ -0,0 +1,16 @@
+$("document").ready(function() {
+    var seinfeld = document.getElementById('seinfeld'),
+	toggle = 1;
+
+    $('#jerry').one('click', function() {
+		switch (toggle) {
+			case 0:
+			text.innerHTML = 'Seinfeld Quote of the day';
+			toggle = 1
+			break;
+		
+		}
+	});
+});
+    
+    
